9c877ca09cbc4ec4852a4e971e18cee304dbc344
Eric <eric@MacBook-Pro.local>
2019-04-20 15:10:12 -0700

    added in the basic higher order containers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Layout from './Containers/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Layout>
+          
+        </Layout>
       </div>
     );
   }
